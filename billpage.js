91ee8abf308928d5c986bed9257d645454171671
yugeshcoder <143157324+yugeshcoder@users.noreply.github.com>
2024-08-16 12:41:08 +0530

    Update billpage.js

--- billpage.js
+++ billpage.js
@@ -33,7 +33,7 @@ billpagescreenshot.addEventListener('click', () => {
     .then(canvas => {
       const url = canvas.toDataURL('image/png');
       const a = document.createElement('a');
-      a.setAttribute('download','imageName.png${rn}');
+      a.setAttribute('download',`imageName.png${rn}`);
       a.setAttribute('href',url);
       a.click();
     })
